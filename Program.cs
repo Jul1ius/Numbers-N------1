626fd20eece0123831288ef0a4407b9b5ec43ce6
its julii <nanji828@gmail.com>
2022-11-29 20:12:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Numbers_N_______1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
